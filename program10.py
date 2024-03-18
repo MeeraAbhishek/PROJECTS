c1b477c9b47779bf2561b722381417f1ddf48f96
MeeraAbhishek <163812183+MeeraAbhishek@users.noreply.github.com>
2024-03-18 11:47:38 +0530

    Add files via upload

---
+++ program10.py
@@ -0,0 +1,9 @@
+# from tkinter import*
+# root=Tk()
+
+# Label=Label(root,text="hello")
+# Label.pack()
+# but=Button(root,text="ok",bg="blue",fg="white")
+# but.pack()
+
+# root.mainloop()
---
+++ program2.py
@@ -0,0 +1,8 @@
+# places=["kochi","munnar","alappuzha","wayanad","kozhikode","kumarakam","thirssur","kovalam"]
+# places.pop(5)
+# print(places)
+# places.insert(4,"idukki")
+# print(places)
+# print(places[4])
+# places.append("periyar")
+# print(places)
